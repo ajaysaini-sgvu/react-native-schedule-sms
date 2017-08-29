198b80ab9988bc4e0ce5038f77f76fb95115d464
Ajay <ajaysaini.sgvu@gmail.com>
2017-08-30 00:07:04 +0530

    Add schedule SMS sample

---
+++ SchduleSMSDemo/index.android.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Button,
+  View
+} from 'react-native';
+import Scheduler from 'react-native-schedule-sms'
+
+export default class SchduleSMSDemo extends Component {
+
+  constructor(props) {
+    super(props);
+    this._pressSendMessage = this._pressSendMessage.bind(this);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Button
+        onPress={() => this._pressSendMessage}
+        title="Send Message"
+        color="#841584"
+        />
+      </View>
+    );
+  }
+
+  _pressSendMessage() {
+    Scheduler.sendMessage("","","");
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('SchduleSMSDemo', () => SchduleSMSDemo);
